c9c81d68a3dc25c12f8d410ccff878ce432d87ba
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2023-08-20 15:27:24 +0530

    update schema to accomodate new requirements

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,9 @@
+
 export default [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::cors',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
