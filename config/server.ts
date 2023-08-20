d348bd03d3e99108c13a42edaabba7c6a06bd526
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2023-08-21 03:18:29 +0530

    host change

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', 'akshay-99h.codes'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', process.env.PORT),
   app: {
     keys: env.array('APP_KEYS'),
